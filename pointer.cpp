b11971d837ba5e7b9755bec1828132b3dff23286
scandid-012 <soltanasabrin@gmail.com>
2025-05-08 20:20:00 +0600

    Update pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -14,14 +14,12 @@ void update(int *a, int *b){
 }
 
 int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+   
     int a, b;
     int *pa = &a, *pb = &b;
     
-    //scanf("%d %d", &a, &b);
     cin>>a>>b;
     update(pa, pb);
-    // printf("%d\n%d", a, b);
     cout<<a<<endl;
     cout<<b;
 
